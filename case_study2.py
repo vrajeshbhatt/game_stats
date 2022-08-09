2953f0ffe940335313397fcc8bc30a4d9672d081
vrajeshbhatt <vrajbhatt005@gmail.com>
2022-08-09 15:08:22 -0400

    some changes

--- case_study2.py
+++ case_study2.py
@@ -1,3 +1,4 @@
+
 import base64
 import json
 import sys
@@ -55,116 +56,116 @@ set_background(r'bg_img.jpg')
 # Reading the dictionary stored in the file
 
 
-print("\n############### GAME STATS PROGRAM ###############")
+# print("\n############### GAME STATS PROGRAM ###############")
 
-def opt1():
-    while(True):
-        x=input("\nEnter a player name or enter 'exit' to return to options menu: ").lower().strip()
-        if(x in pl_dict.keys()):
-            print("Wins\t:",pl_dict[x]["Wins"])
-            print("Losses\t:",pl_dict[x]["Losses"])
-            print("Ties\t:",pl_dict[x]["Ties"])
-            opt1()
+# def opt1():
+#     while(True):
+#         x=input("\nEnter a player name or enter 'exit' to return to options menu: ").lower().strip()
+#         if(x in pl_dict.keys()):
+#             print("Wins\t:",pl_dict[x]["Wins"])
+#             print("Losses\t:",pl_dict[x]["Losses"])
+#             print("Ties\t:",pl_dict[x]["Ties"])
+#             opt1()
 
-        elif x=='exit':
-            options()
+#         elif x=='exit':
+#             options()
             
-        else:
-            print("Player does not exist in the list! Kindly enter again.")            
-            opt1()
+#         else:
+#             print("Player does not exist in the list! Kindly enter again.")            
+#             opt1()
 
-def opt2():
-    name = text_input("Enter player name\t: ").lower().strip()
-    while True:
-        # try:
-        #     wins = int(input("Enter player wins\t: ").strip())
-        #     loss = int(input("Enter player losses\t: ").strip())
-        #     ties = int(input("Enter player ties\t: ").strip())
-        #     break
-        # except:
-        #     print("Invalid data entered, Kindly enter numeric data.")
-        try:
-            wins = text_input("Enter player wins\t: ").strip()
-            loss = text_input("Enter player losses\t: ").strip()
-            ties = text_input("Enter player ties\t: ").strip()
-            break
-        except:
-            st.write("Invalid data entered, Kindly enter numeric data.")
+# def opt2():
+#     name = text_input("Enter player name\t: ").lower().strip()
+#     while True:
+#         # try:
+#         #     wins = int(input("Enter player wins\t: ").strip())
+#         #     loss = int(input("Enter player losses\t: ").strip())
+#         #     ties = int(input("Enter player ties\t: ").strip())
+#         #     break
+#         # except:
+#         #     print("Invalid data entered, Kindly enter numeric data.")
+#         try:
+#             wins = text_input("Enter player wins\t: ").strip()
+#             loss = text_input("Enter player losses\t: ").strip()
+#             ties = text_input("Enter player ties\t: ").strip()
+#             break
+#         except:
+#             st.write("Invalid data entered, Kindly enter numeric data.")
 
-    pl_dict[name] = {'Wins':wins, 'Losses':loss, 'Ties':ties}
-    st.write('Player info added!')
-    # print('Player info added!')
-    # options()
+#     pl_dict[name] = {'Wins':wins, 'Losses':loss, 'Ties':ties}
+#     st.write('Player info added!')
+#     # print('Player info added!')
+#     # options()
 
-def opt3():
-    name=input("\nEnter a player name to delete from records or enter 'exit' to return to options menu: ").lower().strip()
-    if name=='exit':
-        options()
+# def opt3():
+#     name=input("\nEnter a player name to delete from records or enter 'exit' to return to options menu: ").lower().strip()
+#     if name=='exit':
+#         options()
     
-    if(name in pl_dict.keys()):
-        confirm = input("Are you sure? enter 'yes' to proceed or 'exit' to return to options menu: ").lower().strip()
-        if confirm=='exit':
-            options()
+#     if(name in pl_dict.keys()):
+#         confirm = input("Are you sure? enter 'yes' to proceed or 'exit' to return to options menu: ").lower().strip()
+#         if confirm=='exit':
+#             options()
             
-        elif confirm=='yes':
-            del pl_dict[name]
-            print(f"Player {name} record deleted! \nReturning to options menu...")
-            options()
+#         elif confirm=='yes':
+#             del pl_dict[name]
+#             print(f"Player {name} record deleted! \nReturning to options menu...")
+#             options()
 
-        elif x=='exit':
-            options()
+#         elif x=='exit':
+#             options()
             
-        else:
-            print("Player does not exist in the list! Kindly enter again.")
-            opt3()
+#         else:
+#             print("Player does not exist in the list! Kindly enter again.")
+#             opt3()
 
     
-def options():
-    # Printing sorted names
-    print("\nPlayers list: ")
-    for key in sorted(pl_dict.keys()):
-        print("\t\t",key.capitalize())    
+# def options():
+#     # Printing sorted names
+#     print("\nPlayers list: ")
+#     for key in sorted(pl_dict.keys()):
+#         print("\t\t",key.capitalize())    
 
-    print("\nOptions: ")
-    print("1. View specific player's detailed stats")
-    print("2. Add a player to the list")
-    print("3. Delete a player from the list")
-    print("4. Credits")
-    print("5. Exit")
+#     print("\nOptions: ")
+#     print("1. View specific player's detailed stats")
+#     print("2. Add a player to the list")
+#     print("3. Delete a player from the list")
+#     print("4. Credits")
+#     print("5. Exit")
 
-    while True:
-        opt = input('\nEnter option number: ')
-        if opt == '1':
-            opt1()
-            options()
+#     while True:
+#         opt = input('\nEnter option number: ')
+#         if opt == '1':
+#             opt1()
+#             options()
             
-        elif opt == '2':
-            opt2()
-            options()
+#         elif opt == '2':
+#             opt2()
+#             options()
 
-        elif opt == '3':
-            opt3()
-            options()
+#         elif opt == '3':
+#             opt3()
+#             options()
 
-        elif opt == '4':
-            print('\n2022S-T1 AISC1000 - Python 01\nWeek 10 - Case Study 2')
-            print('Course Professor - Mr. Qasim Ali\n')
-            credits_df = pd.DataFrame({'Member name':['Dalveer Kaur', 'Priyanka Sharma', 'Saghithya S. Nateson','Surya Bansal','Vrajeshkumar Bhatt'],
-                                       'ID':[500208166,500208166,500203662,500210660,500209114],
-                                       'Tasks':['Coding','CLI Formatting','File Handling','Coding','Web GUI'],
-                                       'Contribution':['20%','20%','20%','20%','20%']})
+#         elif opt == '4':
+#             print('\n2022S-T1 AISC1000 - Python 01\nWeek 10 - Case Study 2')
+#             print('Course Professor - Mr. Qasim Ali\n')
+#             credits_df = pd.DataFrame({'Member name':['Dalveer Kaur', 'Priyanka Sharma', 'Saghithya S. Nateson','Surya Bansal','Vrajeshkumar Bhatt'],
+#                                        'ID':[500208166,500208166,500203662,500210660,500209114],
+#                                        'Tasks':['Coding','CLI Formatting','File Handling','Coding','Web GUI'],
+#                                        'Contribution':['20%','20%','20%','20%','20%']})
 
-            print(credits_df)
+#             print(credits_df)
             
-        elif opt == '5' :
-            print("Saving updated player dict...")
-            json.dump(pl_dict, open("player_stats.txt",'w'))
-            print("Good Bye!")
+#         elif opt == '5' :
+#             print("Saving updated player dict...")
+#             json.dump(pl_dict, open("player_stats.txt",'w'))
+#             print("Good Bye!")
 
-            sys.exit()
+#             sys.exit()
 
-        else:
-            print("Invalid input! Enter again... ")
+#         else:
+#             print("Invalid input! Enter again... ")
 
 pl_dict = json.load(open(r"player_stats.txt"))
 st.session_state.data = pl_dict
@@ -195,17 +196,25 @@ with tab2:
             loss = cols[1].number_input("Enter player losses\t: ",min_value = 0,step = 1)
             ties = cols[1].number_input("Enter player ties\t: ",min_value = 0,step = 1)
             submitted = st.form_submit_button(label="Submit")
+
             # except:
             #     st.write("Invalid data entered, Kindly enter numeric data.")
 
         if submitted:
-            pl_dict = st.session_state.data
-            pl_dict[name] = {'Wins':wins, 'Losses':loss, 'Ties':ties}
+            print(type(name))
+            if len(name) == 0:
+                st.warning('You can not leave name empty')
+            elif name.isnumeric() == True:
+                st.warning('Invalid input, please try it again')
+            
+            else:
+                pl_dict = st.session_state.data
+                pl_dict[name] = {'Wins':wins, 'Losses':loss, 'Ties':ties}
 
-            json.dump(pl_dict, open(r"player_stats.txt",'w'))
-            st.session_state.data = pl_dict
-            st.success('Player '+ name +' added successfully!')
-            st.balloons()
+                json.dump(pl_dict, open(r"player_stats.txt",'w'))
+                st.session_state.data = pl_dict
+                st.success('Player '+ name +' added successfully!')
+                st.balloons()
 
     delete = st.expander("Delete player")
     with delete:
